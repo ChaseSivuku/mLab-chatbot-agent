3e2e6738f8053d6d964755e8ccb7cb96865e0f7b
Musiki8213 <sithomolamusiki@gmail.com>
2026-02-04 09:05:13 +0200

    Resolve package-lock.json merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import './App.css'
+import { useState } from 'react';
+import ChatbotModal from './containers/ChatbotModal';
 
 function App() {
- 
+  const [isChatOpen, setIsChatOpen] = useState(false);
+
   return (
-    <>
-      HOME
-    </>
-  )
+    <div className="min-h-screen bg-zinc-50 flex flex-col items-center justify-center">
+     
+      {/* The trigger button */}
+      <button 
+        onClick={() => setIsChatOpen(true)}
+        className="bg-[#003d4d] text-white px-6 py-3 rounded-full font-medium hover:bg-opacity-90 transition-all"
+      >
+        Open Chatbot
+      </button>
+
+      {/* The Modal Component */}
+      <ChatbotModal 
+        isOpen={isChatOpen} 
+        onClose={() => setIsChatOpen(false)} 
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
