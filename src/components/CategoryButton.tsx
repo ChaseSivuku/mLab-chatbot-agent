847da15ad9cd48ce824e465265304ede47ef927d
Musiki8213 <sithomolamusiki@gmail.com>
2026-02-09 09:31:08 +0200

    Enhance CategoryButton interaction logic

--- src/components/CategoryButton.tsx
+++ src/components/CategoryButton.tsx
@@ -7,8 +7,9 @@ const CategoryButton = ({ label, onClick }: CategoryButtonProps) => {
   return (
     <button
       onClick={onClick}
-      className="py-2 px-5 bg-white border border-[#A6CE39] rounded-xl text-blackfont-medium hover:border-[#A6CE39] hover:bg-[#A6CE39] hover:shadow-sm transition-all text-center"
+      className="py-2 px-5 bg-white border cursor-pointer border-[#A6CE39] rounded-xl text-blackfont-medium hover:border-[#A6CE39] hover:bg-[#A6CE39] hover:shadow-sm transition-all text-center"
     >
+     
             {label}
           
     </button>
