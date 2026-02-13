0574f3c8c3d8c65bdbe672f6551e51806dd2ed92
Musiki8213 <sithomolamusiki@gmail.com>
2026-02-13 14:27:22 +0200

    Refine CategoryButton interaction and styling

--- src/components/CategoryButton.tsx
+++ src/components/CategoryButton.tsx
@@ -7,11 +7,9 @@ const CategoryButton = ({ label, onClick }: CategoryButtonProps) => {
   return (
     <button
       onClick={onClick}
-      className="py-2 px-5 bg-white border cursor-pointer border-[#A6CE39] rounded-xl text-blackfont-medium hover:border-[#A6CE39] hover:bg-[#A6CE39] hover:shadow-sm transition-all text-center"
+      className="py-2 px-3 sm:px-4 md:px-5 bg-white border cursor-pointer border-[#A6CE39] rounded-xl text-black font-medium hover:border-[#A6CE39] hover:bg-[#A6CE39] hover:shadow-sm transition-all text-center text-xs sm:text-sm md:text-base"
     >
-     
-            {label}
-          
+      {label}
     </button>
   );
 };
